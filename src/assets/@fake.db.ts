a8ad10a2fb4435c3d99d9c4038c146a2a6fe852d
Harun Doğdu <Harundogdu06@gmail.com>
2023-10-10 15:21:14 +0300

    feat: fake db created

---
+++ src/assets/@fake.db.ts
@@ -0,0 +1,52 @@
+const users = [
+  {
+    id: 1,
+    name: "John",
+    age: 20,
+    job: "Developer",
+    hobbies: ["Coding", "Gaming"],
+    address: { city: "Seoul", country: "Korea" },
+    friends: [
+      { name: "Jane", age: 20 },
+      { name: "Bob", age: 21 },
+    ],
+  },
+  {
+    id: 2,
+    name: "Jane",
+    age: 21,
+    job: "Designer",
+    hobbies: ["Designing", "Gaming"],
+    address: { city: "Seoul", country: "Korea" },
+    friends: [
+      { name: "John", age: 20 },
+      { name: "Bob", age: 21 },
+    ],
+  },
+  {
+    id: 3,
+    name: "Bob",
+    age: 22,
+    job: "Marketer",
+    hobbies: ["Marketing", "Gaming"],
+    address: { city: "Seoul", country: "Korea" },
+    friends: [
+      { name: "John", age: 20 },
+      { name: "Jane", age: 21 },
+    ],
+  },
+  {
+    id: 4,
+    name: "Alice",
+    age: 23,
+    job: "Manager",
+    hobbies: ["Managing", "Gaming"],
+    address: { city: "Seoul", country: "Korea" },
+    friends: [
+      { name: "John", age: 20 },
+      { name: "Jane", age: 21 },
+    ],
+  },
+];
+
+export { users };
