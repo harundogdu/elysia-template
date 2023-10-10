2e2f414e9a9cda1a0b9aaebadaaa97cce1221ce2
Harun Doğdu <Harundogdu06@gmail.com>
2023-10-10 15:49:48 +0300

    feat: add new user method created

--- src/services/userServices.ts
+++ src/services/userServices.ts
@@ -1,6 +1,32 @@
 import { users } from "../assets/@fake.db";
 
 const getUserById = (id: number | string) =>
-  users.find((user) => Number(user.id) === (Number(id) || 0));
+  users.find((user: User) => Number(user.id) === (Number(id) || 0));
 
-export { getUserById };
+const addNewUser = (user: User) => {
+  const isExistUser = users.find((_user) => _user.username === user.username);
+  if (isExistUser) {
+    return {
+      message: "Username already exists",
+      status: false,
+    };
+  }
+
+  const lastInsertId = users.sort(
+    (prev: User, next: User) => prev.id - next.id
+  )[users.length - 1].id;
+
+  const newUser: User = {
+    ...user,
+    id: lastInsertId + 1,
+  };
+
+  const _users = [...users, newUser];
+  return {
+    message: "User created successfully",
+    status: true,
+    users: _users,
+  };
+};
+
+export { addNewUser, getUserById };
