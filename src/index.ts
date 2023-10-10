48a41253eb0d37326e97ccce569b5c20781d5a79
Harun Doğdu <Harundogdu06@gmail.com>
2023-10-10 15:13:38 +0300

    feat: get user by id method and routes defined

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,23 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello World!").listen(3000);
+const users = [
+  { id: 1, name: "John" },
+  { id: 2, name: "Jane" },
+  { id: 3, name: "Bob" },
+];
+
+const getUserById = (id: number | string) =>
+  users.find((user) => Number(user.id) === (Number(id) || 0));
+
+const app = new Elysia()
+  .get("/", () => "Hello World!")
+  .get("/test", () => "Hello Test!")
+  .post("/test", () => "Hello Test From Post!")
+  .get("/id/:id", ({ params: { id } }) => {
+    const user = getUserById(id);
+    return user ? user : "User not found";
+  })
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
